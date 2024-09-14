ae85ff6d980c53f33fb651051d75182fe4c25d0d
argulusyn <argulusyn@gmail.com>
2024-09-14 11:50:04 +0200

    feat: add AppHeader

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
+import AppHeader from '@/components/global/AppHeader.vue';
 </script>
 
 <template>
+  <AppHeader />
+
   <RouterView />
 </template>
 
