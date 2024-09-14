ddccd83b3beadb5375a884cd6aeb6d633547d3b5
argulusyn <argulusyn@gmail.com>
2024-09-14 12:31:35 +0200

    feat: add Questionnaire page

---
+++ src/pages/QuestionnairePage.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import ProgressBar from '@/components/common/ProgressBar.vue';
+</script>
+
+<template>
+  <main class="question-page">
+    <ProgressBar class="question-page__progress" :value="21" />
+
+    <div class="question-page__content">
+      <RouterView />
+    </div>
+  </main>
+</template>
+
+<style scoped lang="scss">
+.question-page {
+  width: 100%;
+  height: 100%;
+
+  &__progress {
+    position: fixed;
+    top: 0;
+    left: 0;
+  }
+
+  &__content {
+    padding: 3rem 10rem;
+  }
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'questionnaire-page',
+      component: () => import('@/pages/QuestionnairePage.vue'),
+      children: [],
+    },
+  ],
 });
 
 export default router;
